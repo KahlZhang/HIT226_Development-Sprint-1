ffd33bf4421e7926d0c7bbd706ce2a35be9c3f3f
Jeff123Kam <48338972+Jeff123Kam@users.noreply.github.com>
2019-05-17 14:14:39 +0930

    Update script.js

--- script.js
+++ script.js
@@ -19,4 +19,28 @@ $(document).ready(function () {
     });
 
 
+
 });
+
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
+
+function find() {
+    var oBox = document.getElementById("find_page");
+    var oContent = oBox.innerHTML;
+    var testTxt = document.getElementById("search_input");
+    var oBtn = document.getElementById("btn");
+    var val = testTxt.value;
+    var findText = oContent.split(val);
+    if (val){
+        oBox.innerHTML = findText.join('<span style="color:red;">' + val + '</span> ');
+    }
+    else {}
+}
